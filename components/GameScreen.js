fbae7e05c4d25343f4093665563e0a0a347f0013
Ben <benpong89@gmail.com>
2018-11-02 15:09:18 -0400

    style game page, instructions, next word button

--- components/GameScreen.js
+++ components/GameScreen.js
@@ -13,9 +13,6 @@ class DetailsScreen extends Component {
     title: "Game"
   };
 
-  //Word library should eventually be passed in as props instead of local state.
-  //Use local state for user TextInput
-
   state = {
     Library: {
       Test: ["exam", "quiz", "assessment", "evaluation"],
@@ -23,7 +20,8 @@ class DetailsScreen extends Component {
       Happy: ["cheerful", "contented", "delighted", "ecstatic"]
     },
     DisplayWord: "",
-    DisplayInputs: new Set(),
+    SeenWords: new Set(),
+    DisplayInputs: [],
     TextInput: "",
     Score: 0,
     Message: ""
@@ -33,22 +31,22 @@ class DetailsScreen extends Component {
     this.setState({ TextInput: text });
   };
 
-  checkWord() {
-    const word = this.state.TextInput;
+  async checkWord() {
+    const word = this.state.TextInput.toLowerCase();
     const synonyms = this.state.Library[this.state.DisplayWord];
+    // if (this.state.DisplayInputs.has(word)) {
+    //   this.setState({ Message: "Already submitted!" });
+    // }
 
-    if (
-      synonyms.includes(word) &&
-      this.state.DisplayInputs.has(word) === false
-    ) {
-      this.setState({
+    if (synonyms.includes(word)) {
+      await this.setState({
         Score: this.state.Score + 1,
-        DisplayInputs: this.state.DisplayInputs.add(word),
+        DisplayInputs: this.state.DisplayInputs.concat(word),
         Message: "Good Job! See if you can get more words",
         TextInput: ""
       });
     } else {
-      this.setState({ Message: "Not a synonym, try again" });
+      this.setState({ Message: "Not a valid entry try again!" });
     }
     //Reference the list of synonyms based on the DisplayWord local state,
     //Check for inclusion of word submitted.
@@ -56,31 +54,33 @@ class DetailsScreen extends Component {
   }
 
   componentDidMount() {
-    const words = Object.keys(this.state.Library);
+    this.setWord();
+  }
+
+  setWord() {
+    const words = Object.keys(this.state.Library).filter(
+      word => !this.state.SeenWords.has(word)
+    );
     const word = words[Math.floor(Math.random() * words.length)];
     this.setState({
-      DisplayWord: word
+      DisplayWord: word,
+      SeenWords: this.state.SeenWords.add(word)
     });
   }
 
   render() {
+    const inputs = this.state.DisplayInputs.map((word, idx) => {
+      return <Text key={idx}>{word + " "}</Text>;
+    });
+
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View style={styles.container}>
         <Text>Game Screen !!</Text>
+        <Text>Synonyms: {inputs}</Text>
         <Text>Score: {this.state.Score}</Text>
-        //Display random word from state libary //SetState for the display word
-        //When and how to select random word. Do I set displayword state too?
-        <Text>{this.state.DisplayWord}</Text>
+        <Text style={styles.displayWord}>{this.state.DisplayWord}</Text>
         <TextInput
-          style={{
-            margin: 10,
-            width: 200,
-            height: 60,
-            borderColor: "#7a42f4",
-            borderWidth: 1,
-            padding: 15,
-            fontSize: 25
-          }}
+          style={styles.textInput}
           underlineColorAndroid="transparent"
           placeholder="Synonym here"
           placeholderTextColor="#9a73ef"
@@ -89,16 +89,20 @@ class DetailsScreen extends Component {
         />
         <Text>{this.state.Message}</Text>
         <TouchableOpacity
-          style={{
-            backgroundColor: "#7a42f4",
-            padding: 10,
-            margin: 15,
-            height: 40
-          }}
+          style={styles.button}
           onPress={() => this.checkWord()}
         >
           <Text style={{ color: "white" }}>Submit</Text>
         </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => this.setWord()}>
+          <Text style={{ color: "white" }}>Next Word</Text>
+        </TouchableOpacity>
+        <Text style={styles.instructions}>
+          How to Play: Type in a Synonym of the word in bold. For each synonym
+          you get correctly, you get a score. Skip to next word after you get 5
+          words. If you skip to next word before you reach 5 you will lose a
+          points. Time limit is 60 seconds
+        </Text>
         <Button
           title="Go back"
           onPress={() => this.props.navigation.goBack()}
@@ -107,5 +111,36 @@ class DetailsScreen extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  button: {
+    backgroundColor: "#7a42f4",
+    padding: 10,
+    margin: 15,
+    height: 40
+  },
+  instructions: {
+    margin: 10,
+    padding: 5
+  },
+  textInput: {
+    margin: 10,
+    width: 200,
+    height: 60,
+    borderColor: "#7a42f4",
+    borderWidth: 1,
+    padding: 15,
+    fontSize: 25
+  },
+  displayWord: {
+    margin: 10,
+    fontSize: 25
+  }
+});
 
 export default DetailsScreen;
